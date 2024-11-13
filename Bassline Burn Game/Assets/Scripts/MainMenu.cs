fccbd2d4463d562081da7f1400a080f074e3d466
Jack Martin <tus40499@temple.edu>
2024-11-13 14:47:23 -0500

    Added Lobby Scene with buttons/labels

--- Bassline Burn Game/Assets/Scripts/MainMenu.cs
+++ Bassline Burn Game/Assets/Scripts/MainMenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
     }
     public void PlayOnline()
     {
-         SceneManager.LoadScene("OnlineMultiplayerScene");
+         SceneManager.LoadScene("Lobby");
     }
 
     public void OpenOptions()
